168fdb6a7bbdbb9902077073a28f078e1a94c064
Chris Bourke <cbourke@cse.unl.edu>
2022-04-26 16:39:32 -0500

    fixed immutable list problem; added hint outline

--- src/main/java/unl/cse/HeapSort.java
+++ src/main/java/unl/cse/HeapSort.java
@@ -1,5 +1,6 @@
 package unl.cse;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -18,14 +19,21 @@ public class HeapSort {
 	 */
 	public static <T> void heapSort(List<T> list, Comparator<T> comp) {
 
-		// TODO: implement this using
+		// TODO: implement this using a PriorityQueue
+		// Outline:
+		//   For each element x in list:
+		//     remove x from list
+		//     add x to heap
+		//   While heap is not empty:
+		//     remove the min element (poll)
+		//     add it to list
 
 		return;
 	}
 
 	public static void main(String args[]) {
 
-		List<Integer> input = Arrays.asList(9, 4, 6, 2, 7, 8, 1, 3, 5);
+		List<Integer> input = new ArrayList<>(Arrays.asList(9, 4, 6, 2, 7, 8, 1, 3, 5));
 		HeapSort.heapSort(input, INT_CMP);
 		System.out.println(input);
 	}
